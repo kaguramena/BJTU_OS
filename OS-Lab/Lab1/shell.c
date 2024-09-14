80828b1a97c807e56a794a2f5e8be7826b6228ce
kagu <22281110@bjtu.edu.cn>
2024-09-14 17:33:01 +0000

    Finish read and parser

--- OS-Lab/Lab1/shell.c
+++ OS-Lab/Lab1/shell.c
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define BUF_SIZE 1024
+#define ARG_DELIM " \n\t\a\r"
 #define MAX_ARGS 5
 
 extern int _cd(int argc,char* argv[]);
@@ -15,28 +17,57 @@ extern int _pstree(int argc,char* argv[]);
 extern int _ls(int argc,char* argv[]);
 
 
+char* read_command_line(void)
+{
+    char* command = NULL;
+    ssize_t buf_size = 0;
+
+    if (getline(&command, &buf_size, stdin) == -1){
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // We recieved an EOF
+    } else  {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return command;
+}
 
 // 这个函数将命令字符串按空格分割并存储到 argv 数组中
-int parse_command(char *command, char *argv[]) {
-    int argc = 0;
-    char *token = strtok(command, " \t\n");  // 用空格、Tab 或换行符分割命令
+char **parse_command_line(char* command,int* argc)
+{
+    int bufsize = BUF_SIZE;
+    char** tokens;
+    char* token;
+    tokens = (char**)malloc(bufsize * sizeof(char*));
 
-    while (token != NULL && argc < MAX_ARGS) {
-        if(strncmp(token,"$",1) == 0)
-        {
-            char s[64];
-            strcpy(s,token + 1);
-            argv[argc++] = getenv(s);
-        }
-        else
-        {
-            argv[argc++] = token;  // 将分割后的部分存储到 argv 数组中
-        }
-        token = strtok(NULL, " \t\n");  // 继续分割剩余部分
+    if(!tokens)
+    {
+        perror("MALLOC ERROR");
+        exit(EXIT_FAILURE);
     }
-
-    argv[argc] = NULL;  // 以 NULL 结束参数列表（方便 exec 系列函数调用）
-    return argc;
+    int position = 0;
+    token = strtok(command,ARG_DELIM);
+    while(token != NULL)
+    {
+        tokens[position] = token;
+        position ++;
+        if(position >= BUF_SIZE)
+        {
+            bufsize += BUF_SIZE;
+            tokens = realloc(tokens,bufsize * sizeof(char*));
+            if(!tokens)
+            {
+                perror("REALLOC ERROR");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL,ARG_DELIM);
+    }
+    tokens[position] = NULL;
+    *argc = position - 1;
+    return tokens;
 }
 
 
@@ -47,15 +78,17 @@ int main()
     {
         char* s = getenv("PWD");
         int argc = 0;
-        char* argv[4];
+        char** argv;
         strcpy(_PWD,s);
         printf("kagu@kagu:%s$ ",_PWD);
-        char _COMMAND[128];
-        fgets(_COMMAND,sizeof(_COMMAND),stdin); // get user input
-        //fputs(_COMMAND,stdout); Debug Need it 
-
+        //printf("1");
+        char* _COMMAND = read_command_line();
+        
+        //fputs(_COMMAND,stdout); //Debug Need it 
+        argv = parse_command_line(_COMMAND,&argc);
+        
         _COMMAND[strcspn(_COMMAND, "\n")] = 0;
-        argc = parse_command(_COMMAND,argv);
+        
         if(!strncmp(_COMMAND,"cd",2)) // so far,cd only support 2 argue
         {
             _cd(argc,argv);
@@ -65,6 +98,7 @@ int main()
         }
         else if(!strncmp(_COMMAND,"ls",2))
         {
+            
             _ls(argc,argv);
         }
         else if(!strncmp(_COMMAND,"pstree",6))
@@ -83,7 +117,7 @@ int main()
         {
             //调用外部命令
             pid_t pid = fork();
-                if (pid < 0) {
+            if (pid < 0) {
                 // fork 失败
                 perror("fork failed");
                 exit(1);
@@ -100,9 +134,13 @@ int main()
             else {
                 // 父进程，等待子进程结束
                 int status;
-                waitpid(pid, &status, 0);
+                do
+                {
+                    waitpid(pid, &status, 0);
+                }while(!WIFSIGNALED(status) | !WIFEXITED(status));
             }
         }
+        free(_COMMAND);
     }
     
 }
